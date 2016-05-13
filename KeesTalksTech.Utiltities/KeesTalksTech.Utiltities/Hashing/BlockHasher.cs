c22b23e5c6de4775403e29e88a5b3764984e4346
Kees C. Bakker <c.bakker@gmx.net>
2016-05-13 17:59:05 +0200

    BlockHasher - helps with block hashing

---
+++ KeesTalksTech.Utiltities/KeesTalksTech.Utiltities/Hashing/BlockHasher.cs
@@ -0,0 +1,206 @@
+﻿namespace KeesTalksTech.Utiltities.Hashing
+{
+	/* The MIT License (MIT)
+	 *
+	 * Copyright (c) 2016 Kees C. Bakker, @KeesTalksTech, www.keestalkstech.com
+	 * 	
+	 * Permission is hereby granted, free of charge, to any person obtaining a copy
+	 * of this software and associated documentation files (the "Software"), to deal
+	 * in the Software without restriction, including without limitation the rights
+	 * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	 * copies of the Software, and to permit persons to whom the Software is
+	 * furnished to do so, subject to the following conditions:
+	 * 
+	 * The above copyright notice and this permission notice shall be included in all
+	 * copies or substantial portions of the Software.
+	 * 
+	 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	 * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	 * SOFTWARE.
+	 */
+
+	using System;
+	using System.IO;
+	using System.Security.Cryptography;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// Helps with block hashing.
+	/// </summary>
+	public class BlockHasher : IDisposable
+	{
+		private HashAlgorithm _hasher;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BlockHasher"/> class.
+		/// </summary>
+		/// <param name="name">The name.</param>
+		public BlockHasher(string name)
+		{
+			if (String.IsNullOrEmpty(name))
+			{
+				throw new ArgumentNullException("name");
+			}
+
+			_hasher = HashAlgorithm.Create(name);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BlockHasher"/> class.
+		/// </summary>
+		/// <param name="algorithm">The algorithm.</param>
+		public BlockHasher(HashAlgorithm algorithm)
+		{
+			if (algorithm == null)
+			{
+				throw new ArgumentNullException("algorithm");
+			}
+
+			_hasher = algorithm;
+		}
+
+		/// <summary>
+		/// Indicates how to format the string.
+		/// </summary>
+		public enum StringFormat
+		{
+			Hexadecimal,
+			Base64
+		}
+
+		/// <summary>
+		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+		/// </summary>
+		public void Dispose()
+		{
+			_hasher.Dispose();
+		}
+
+		/// <summary>
+		/// Gets the hash.
+		/// </summary>
+		/// <returns>A byte array with the hash.</returns>
+		public byte[] GetHash()
+		{
+			_hasher.TransformFinalBlock(new byte[0], 0, 0);
+			return _hasher.Hash;
+		}
+
+		/// <summary>
+		/// Gets the string hash.
+		/// </summary>
+		/// <param name="format">The format.</param>
+		/// <returns>The hash.</returns>
+		public string GetStringHash(StringFormat format = StringFormat.Hexadecimal)
+		{
+			var hash = GetHash();
+
+			switch (format)
+			{
+				case StringFormat.Hexadecimal:
+
+					{
+						string result = "";
+						for (int i = 0; i < hash.Length; i++)
+						{
+							result += hash[i].ToString("x2");
+						}
+						return result;
+					}
+
+				case StringFormat.Base64:
+					{
+						return Convert.ToBase64String(hash);
+					}
+				default:
+					{
+
+						throw new NotSupportedException();
+					}
+			}
+		}
+
+		/// <summary>
+		/// Transforms the specified stream.
+		/// </summary>
+		/// <param name="stream">The stream.</param>
+		/// <param name="bufferSize">Size of the buffer.</param>
+		public void Transform(Stream stream, int bufferSize = 16 * 1024)
+		{
+			if (stream == null)
+			{
+				throw new ArgumentNullException("stream");
+			}
+
+			byte[] buffer = new byte[bufferSize];
+			int read;
+
+			while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				Transform(buffer, 0, read);
+			}
+		}
+
+		/// <summary>
+		/// Transforms the specified string.
+		/// </summary>
+		/// <param name="str">The string.</param>
+		public void Transform(string str)
+		{
+			Transform(str, Encoding.UTF8);
+		}
+
+		/// <summary>
+		/// Transforms the specified string.
+		/// </summary>
+		/// <param name="str">The string.</param>
+		/// <param name="encoding">The encoding.</param>
+		public void Transform(string str, Encoding encoding)
+		{
+			var bytes = encoding.GetBytes(str);
+			Transform(bytes);
+		}
+
+		/// <summary>
+		/// Transforms the specified buffer.
+		/// </summary>
+		/// <param name="buffer">The buffer.</param>
+		public void Transform(byte[] buffer)
+		{
+			Transform(buffer, 0, buffer.Length);
+		}
+
+		/// <summary>
+		/// Transforms the specified buffer.
+		/// </summary>
+		/// <param name="buffer">The buffer.</param>
+		/// <param name="offset">The offset.</param>
+		/// <param name="length">The length.</param>
+		public void Transform(byte[] buffer, int offset, int length)
+		{
+			_hasher.TransformBlock(buffer, offset, length, null, 0);
+		}
+
+		/// <summary>
+		/// Transforms the asynchronous.
+		/// </summary>
+		/// <param name="stream">The stream.</param>
+		/// <param name="bufferSize">Size of the buffer.</param>
+		/// <returns>The task.</returns>
+		public async Task TransformAsync(Stream stream, int bufferSize = 16 * 1024)
+		{
+			byte[] buffer = new byte[bufferSize];
+			int read;
+
+			while ((read = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+			{
+				Transform(buffer, 0, read);
+			}
+		}
+	}
+}
---
+++ KeesTalksTech.Utiltities/KeesTalksTech.Utiltities/Hashing/BlockHasherTest.cs
@@ -0,0 +1,120 @@
+﻿#if (DEBUG)
+namespace KeesTalksTech.Utiltities.Hashing
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System.IO;
+	using System.Security.Cryptography;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	[TestClass]
+	public class BlockHasherTest
+	{
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public async Task MD5HashString_AsyncStreamTransform_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				var mr = new MemoryStream();
+				var buffer = Encoding.UTF8.GetBytes("Hello world!");
+				mr.Write(buffer, 0, buffer.Length);
+				mr.Position = 0;
+
+				await blockhasher.TransformAsync(mr);
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "86fb269d190d2c85f6e0468ceca42a20", "Invalid MD5 hash result.");
+			}
+		}
+
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void MD5HashString_MultipleTransforms_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				blockhasher.Transform("Hello");
+				blockhasher.Transform(" ");
+				blockhasher.Transform("world");
+				blockhasher.Transform("!");
+
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "86fb269d190d2c85f6e0468ceca42a20", "Invalid MD5 hash result.");
+			}
+		}
+
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void MD5HashString_SingleTransformBuffer_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				var buffer = Encoding.UTF8.GetBytes("Hello world!");
+				blockhasher.Transform(buffer);
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "86fb269d190d2c85f6e0468ceca42a20", "Invalid MD5 hash result.");
+			}
+		}
+
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void MD5HashString_SingleTransformPartialBuffer_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				var buffer = Encoding.UTF8.GetBytes("Hello world!");
+				blockhasher.Transform(buffer, 6, buffer.Length - 6);
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "08cf82251c975a5e9734699fadf5e9c0", "Invalid MD5 hash result.");
+			}
+		}
+
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void MD5HashString_SingleTransformString_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				blockhasher.Transform("Hello world!");
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "86fb269d190d2c85f6e0468ceca42a20", "Invalid MD5 hash result.");
+			}
+		}
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void MD5HashString_StreamTransform_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher("md5"))
+			{
+				var mr = new MemoryStream();
+				var buffer = Encoding.UTF8.GetBytes("Hello world!");
+				mr.Write(buffer, 0, buffer.Length);
+				mr.Position = 0;
+
+				blockhasher.Transform(mr);
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "86fb269d190d2c85f6e0468ceca42a20", "Invalid MD5 hash result.");
+			}
+		}
+
+		[TestCategory("UnitTest")]
+		[TestMethod]
+		public void SHA1HashString_GivenHasherSingleTransform_HexadecimalResult()
+		{
+			using (var blockhasher = new BlockHasher(HashAlgorithm.Create("sha1")))
+			{
+				blockhasher.Transform("Hello world!");
+				var a1 = blockhasher.GetStringHash();
+
+				Assert.AreEqual(a1, "d3486ae9136e7856bc42212385ea797094475802", "Invalid SHA1 hash result.");
+			}
+		}
+	}
+}
+#endif
