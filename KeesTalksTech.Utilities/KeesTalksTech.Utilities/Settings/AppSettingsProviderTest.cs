4ce48b65136e3d9cb035e8779d3e21e128855711
Kees C. Bakker <c.bakker@gmx.net>
2016-05-14 17:50:50 +0200

    AppSettingsProvider: provides app settings from the .config to the application. Can be used to automatically fill settings objects using the convention: {namespace}.{class-name}.{field-name}

---
+++ KeesTalksTech.Utilities/KeesTalksTech.Utilities/Settings/AppSettingsProviderTest.cs
@@ -0,0 +1,47 @@
+﻿namespace KeesTalksTech.Utiltities.Settings
+{
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using System.Configuration;
+
+	class MyTestSettings
+	{
+		public string UserName { get; set; }
+
+		public string Password { get; set; }
+	}
+
+	[TestClass]
+	public class AppSettingsProviderTest
+	{
+		[TestMethod]
+		public void AppSettingsProvider_Create_ObjectSettings()
+		{
+			ConfigurationManager.AppSettings["KeesTalksTech.Utiltities.Settings.MyTestSettings.UserName"] = "Kees C. Bakker";
+			ConfigurationManager.AppSettings["KeesTalksTech.Utiltities.Settings.MyTestSettings.Password"] = "1337!42";
+
+			var s = AppSettingsProvider.Create<MyTestSettings>();
+
+			Assert.AreEqual(s.UserName, "Kees C. Bakker");
+			Assert.AreEqual(s.Password, "1337!42");
+		}
+
+		class MyInnerTestSettings
+		{
+			public string UserName { get; set; }
+
+			public string Password { get; set; }
+		}
+
+		[TestMethod]
+		public void AppSettingsProvider_Create_InnerClassObjectSettings()
+		{
+			ConfigurationManager.AppSettings["KeesTalksTech.Utiltities.Settings.AppSettingsProviderTest.MyInnerTestSettings.UserName"] = "Kees C. Bakker";
+			ConfigurationManager.AppSettings["KeesTalksTech.Utiltities.Settings.AppSettingsProviderTest.MyInnerTestSettings.Password"] = "1337!42";
+
+			var s = AppSettingsProvider.Create<MyInnerTestSettings>();
+
+			Assert.AreEqual(s.UserName, "Kees C. Bakker");
+			Assert.AreEqual(s.Password, "1337!42");
+		}
+	}
+}
