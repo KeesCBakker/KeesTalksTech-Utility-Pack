a73f5dfdf4b3891d249eb8fd04a3f0d431d72616
KeesCBakker <c.bakker@gmx.net>
2016-05-23 22:19:01 +0200

    Changed locking. Removed prefix.

--- KeesTalksTech-Utility-Pack/KeesTalksTech.Utilities/Locking/NamedMonitor.cs
+++ KeesTalksTech-Utility-Pack/KeesTalksTech.Utilities/Locking/NamedMonitor.cs
@@ -9,30 +9,28 @@ namespace KeesTalksTech.Utilities.Locking
     /// A NamedMonitor creates a locking mechanism that will lock on keys. 
     /// </summary>
     public class NamedMonitor
-	{
-		private Dictionary<string, object> _monitors = new Dictionary<string, object>();
-		private object _mutex = new object();
-		private string _prefix = Guid.NewGuid().ToString();
+    {
+        private Dictionary<string, object> _monitors = new Dictionary<string, object>();
 
         /// <summary>
         /// Enters the monitor.
         /// </summary>
         /// <param name="key">The key.</param>
         public void Enter(string key)
-		{
-			var l = GetLockingObject(key);
-			Monitor.Enter(l);
-		}
+        {
+            var l = GetLockingObject(key);
+            Monitor.Enter(l);
+        }
 
         /// <summary>
         /// Exits the monitor.
         /// </summary>
         /// <param name="key">The key.</param>
         public void Exit(string key)
-		{
-			var l = GetLockingObject(key);
-			Monitor.Exit(l);
-		}
+        {
+            var l = GetLockingObject(key);
+            Monitor.Exit(l);
+        }
 
         /// <summary>
         /// Gets the locking object.
@@ -40,21 +38,19 @@ namespace KeesTalksTech.Utilities.Locking
         /// <param name="key">The key.</param>
         /// <returns>The object that can be used for locking.</returns>
         private object GetLockingObject(string key)
-		{
-			lock (_mutex)
-			{
-				key = _prefix + "." + key;
+        {
+            lock (_monitors)
+            {
+                if (!_monitors.ContainsKey(key))
+                {
+                    var l = new object();
+                    _monitors.Add(key, l);
+                    return l;
+                }
 
-				if (!_monitors.ContainsKey(key))
-				{
-					var l = new object();
-					_monitors.Add(key, l);
-					return l;
-				}
-
-				return _monitors[key];
-			}
-		}
+                return _monitors[key];
+            }
+        }
 
         /// <summary>
         /// Executes the function within a monitor.
@@ -64,17 +60,17 @@ namespace KeesTalksTech.Utilities.Locking
         /// <param name="func">The function.</param>
         /// <returns>An async task.</returns>
         public async Task<T> ExecuteWithinMonitor<T>(string key, Func<Task<T>> func)
-		{
-			try
-			{
-				Enter(key);
-				return await func();
-			}
-			finally
-			{
-				Exit(key);
-			}
-		}
+        {
+            try
+            {
+                Enter(key);
+                return await func();
+            }
+            finally
+            {
+                Exit(key);
+            }
+        }
 
         /// <summary>
         /// Executes the function within monitor.
@@ -84,17 +80,17 @@ namespace KeesTalksTech.Utilities.Locking
         /// <param name="func">The function.</param>
         /// <returns>The result.</returns>
         public T ExecuteWithinMonitor<T>(string key, Func<T> func)
-		{
-			try
-			{
-				Enter(key);
-				return func();
-			}
-			finally
-			{
-				Exit(key);
-			}
-		}
+        {
+            try
+            {
+                Enter(key);
+                return func();
+            }
+            finally
+            {
+                Exit(key);
+            }
+        }
 
         /// <summary>
         /// Executes the action within the monitor.
@@ -102,16 +98,16 @@ namespace KeesTalksTech.Utilities.Locking
         /// <param name="key">The key.</param>
         /// <param name="action">The action.</param>
         public void ExecuteWithinMonitor(string key, Action action)
-		{
-			try
-			{
-				Enter(key);
-				action();
-			}
-			finally
-			{
-				Exit(key);
-			}
-		}
+        {
+            try
+            {
+                Enter(key);
+                action();
+            }
+            finally
+            {
+                Exit(key);
+            }
+        }
     }
 }
