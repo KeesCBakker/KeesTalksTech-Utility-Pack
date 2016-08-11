7f77f4ddc2b16efa37e0b4970be3c555350b23bd
KeesCBakker <c.bakker@gmx.net>
2016-08-11 21:04:44 +0200

    Updated information and version info

--- KeesTalksTech-Utility-Pack/KeesTalksTech.Utilities.Settings/Properties/AssemblyInfo.cs
+++ KeesTalksTech-Utility-Pack/KeesTalksTech.Utilities.Settings/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Helps with settings management by wrapping the Configuration Manager.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KeesTalksTech")]
-[assembly: AssemblyProduct("KeesTalksTech Utility Pack")]
+[assembly: AssemblyProduct("Utility Pack - Settings")]
 [assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
